e36be46533a211b146963f342474725e0a921b80
David Lima <david.lima50@etec.sp.gov.br>
2020-03-19 13:55:25 -0300

    Updated sqlite lib
    Updated sqlite lib, implemented new functions

--- src/lib/sqlite.js
+++ src/lib/sqlite.js
@@ -1,5 +1,4 @@
 import * as SQLite from 'expo-sqlite';
-import uuid from 'uuid-random';
 
 /**
  * Init database
@@ -10,6 +9,9 @@ import uuid from 'uuid-random';
  * @param {Object[]} databaseStructure.tables.colls
  * @param {String} databaseStructure.tables.colls.name
  * @param {String[]} databaseStructure.tables.colls.properties
+ * @param {Object[]} databaseStructure.tables.indexes
+ * @param {String[]} databaseStructure.tables.indexes.colls
+ * @param {Boolean=} databaseStructure.tables.indexes.unique
  */
 function DB(databaseStructure) {
     if (typeof databaseStructure != 'object') throw new Error('Invalid database structure');
@@ -18,9 +20,7 @@ function DB(databaseStructure) {
 
     this._db = SQLite.openDatabase(`${databaseStructure.name}.db`);
 
-    for (let table of databaseStructure.tables) {
-        this.createTable(table);
-    }
+    for (let table of databaseStructure.tables) this.createTable(table);
 
     this._databseStructure = databaseStructure;
     return this;
@@ -51,17 +51,20 @@ DB.prototype.executeSql = function (sql, params = []) {
  * @param {Object[]} tableStructure.colls
  * @param {String} tableStructure.colls.name
  * @param {String[]} tableStructure.colls.properties
+ * @param {Object[]} tableStructure.indexes
+ * @param {String[]} tableStructure.indexes.colls
+ * @param {Boolean} tableStructure.indexes.unique
  * @returns {Boolean}
  */
-DB.prototype.createTable = async function (structure) {
-    if (typeof structure != 'object') throw new Error(`Invalid table structure`);
+DB.prototype.createTable = async function (tableStructure) {
+    if (typeof tableStructure != 'object') throw new Error(`Invalid table tableStructure`);
 
     let error;
     let sqlCommand = `create table if not exists '${
-        structure.name || (error = `Invalid table name`)
+        tableStructure.name || (error = `Invalid table name`)
         }' (${
-        Array.isArray(structure.colls)
-            ? structure.colls.map(coll => `${
+        Array.isArray(tableStructure.colls)
+            ? tableStructure.colls.map(coll => `${
                 coll.name || (error = 'Invalid')
                 } ${coll.properties.join(' ')}`)
             : (error = 'Invalid colls')
@@ -70,6 +73,8 @@ DB.prototype.createTable = async function (structure) {
     if (error) throw new Error(error);
 
     let [{ _complete }] = await this.executeSql(sqlCommand);
+
+    for (let { colls, unique } in tableStructure.indexes) await this.createIndex(tableStructure.name, colls, unique);
 
     return _complete;
 };
@@ -83,12 +88,29 @@ DB.prototype.dropTable = async function (name) {
     let [{ _complete }] = await this.executeSql(`drop table if exists '${name}'`);
     return _complete;
 }
+/**
+ * Create an index on table
+ * @param {String} tableName table name
+ * @param {String[]} colls colls to create the index
+ * @param {Boolean=} unique default is false
+ * @returns {Boolean}
+ */
+DB.prototype.createIndex = async function (tableName, colls = [], unique = false) {
+    if (!tableName) throw new Error(`Invalid table name ${tableName}`);
+    if (!Array.isArray(colls)) throw new Error(`Invalid colls, must be array`);
+    if (colls.length < 1) return true;
 
+    let [{ _complete }] = await this.executeSql(
+        `CREATE ${Boolean(unique) ? 'UNIQUE' : ''} INDEX 'index_${colls.join('_')}' ON '${tableName}' ('${colls.join("','")}')`
+    );
+    return _complete;
+}
 /**
  * insert
  * update
  * delete
  * select
+ * BASE ON expo-sqlite-orm
  */
 
 export default DB;
