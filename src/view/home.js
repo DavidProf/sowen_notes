c99793bbf1d383509a0223162be76bb7fc7bc772
David Lima <david.lima50@etec.sp.gov.br>
2020-04-05 00:32:44 -0300

    feat(home.js): backup feat start
    Added multiple options (animated), 'backup' and 'add note' to home screen.
    
    "One step to future, is better than zero steps." (by David Lima, but maybe, before, someone already said that)

--- src/view/home.js
+++ src/view/home.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import {
-    StyleSheet,
     TouchableOpacity,
     View,
-    Image
+    Image,
+    Animated
 } from 'react-native';
 import noteList from './noteList';
 import db from '../lib/sowenNotesDB';
@@ -11,15 +11,39 @@ import styles from './styles';
 
 export default function Home({ navigation }) {
     const [notes, setNotes] = useState([]);
+    const [show, setShow] = useState(false);
+    const [pX] = useState(new Animated.Value(15));
+    const [pY] = useState(new Animated.Value(15));
 
     db.selectAll().then(rows => setNotes(rows));
+
+    function showOptions() {
+        Animated.timing(pX, { toValue: show ? 15 : 90, duration: 250 }).start();
+        Animated.timing(pY, { toValue: show ? 15 : 90, duration: 250 }).start();
+        setShow(!show);
+    }
 
     return (
         <View style={styles.container}>
             {noteList(notes, navigation)}
-            <TouchableOpacity style={styles.roundedButton}
-                onPressOut={() => navigation.navigate('Note')}>
-                <Image source={require('../../assets/add.png')} style={styles.icon} />
+            <Animated.View style={[styles.roundedButton, { right: pX }]}>
+                <TouchableOpacity
+                    onPressOut={() => alert('backup')}>
+                    <Image source={require('../../assets/cloud.png')} style={styles.icon} />
+                </TouchableOpacity>
+            </Animated.View>
+
+
+            <Animated.View style={[styles.roundedButton, { bottom: pY }]}>
+                <TouchableOpacity
+                    onPressOut={() => navigation.navigate('Note')}>
+                    <Image source={require('../../assets/add.png')} style={styles.icon} />
+                </TouchableOpacity>
+            </Animated.View>
+
+            <TouchableOpacity style={[styles.roundedButton]}
+                onPressOut={() => showOptions()}>
+                <Image source={show ? require('../../assets/x.png') : require('../../assets/burguer.png')} style={styles.icon} />
             </TouchableOpacity>
         </View >
     );
