b70b24439c5fd4dbbb308879954406092f860992
David Lima <david.lima50@etec.sp.gov.br>
2020-04-27 09:03:26 -0300

    revert(backup): go back backup feat
    Ok, I don't know why the google sign in don't work and I am tired for now

--- src/view/settings.js
+++ src/view/settings.js
@@ -18,15 +18,6 @@ export default function Settings({ route: { params }, navigation }) {
 
     return (
         <View style={[styles.container, { justifyContent: "flex-start", backgroundColor: theme.colors.background }]}>
-            <TouchableOpacity style={[styles.itemButton, { backgroundColor: theme.colors.itemButton }]}>
-                <Text style={styles.itemButtonText}>Backup</Text>
-                <Image source={icons.upload} style={styles.icon} />
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.itemButton, { backgroundColor: theme.colors.itemButton }]}>
-                <Text style={styles.itemButtonText}>Restore</Text>
-                <Image source={icons.download} style={styles.icon} />
-            </TouchableOpacity>
-
             <TouchableOpacity
                 style={[styles.itemButton, { backgroundColor: theme.colors.itemButton }]}
                 onPress={toggleTheme}>
@@ -38,8 +29,6 @@ export default function Settings({ route: { params }, navigation }) {
 };
 
 const icons = {
-    upload: require('../../assets/upload.png'),
-    download: require('../../assets/download.png'),
     sun: require('../../assets/sun.png'),
     moon: require('../../assets/moon.png'),
 }
